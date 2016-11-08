5e7142780605a6186d581d410e728868ab8f82b6
mgu <Martin.Guski@gmail.com>
2016-11-08 08:59:45 -0900

    rawGUI shows header information now

--- rawGUI.py
+++ rawGUI.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
+# 
+# function to show diagnostic samples of control program
+# usage:
+# 
+#  ./rawGUI.sh                     : load newest file in /data/diagnostic_samples/
+#  ./rawGUI.sh bla.txt.a           : look for this file in /data/diagnostic_samples/
+#  ./rawGUI.sh /a/path/bla.txt.a   : take this file
+
+
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
-
+from matplotlib import gridspec
 
 class RawDataGUI:
     def __init__(self, inputFile):
@@ -15,10 +24,15 @@ class RawDataGUI:
         self.nDatasets = len(self.allDataSets)
         self.nChannels = len(self.channelNames)
         self.fgh       = plt.figure()
-        self.ax        = plt.subplot(111)       
+        gs = gridspec.GridSpec(10,1)
+        self.ax    = self.fgh.add_subplot(gs[0:8,:])
+        self.axTxt = self.fgh.add_subplot(gs[9,:])
+        self.axTxt.axis("off")   
         self.cid       = self.fgh.canvas.mpl_connect('key_press_event', self.keyCallback)
         self.updateGUI()
         print('Shortcuts:\n  left / right : previous / next sequence \n  up / down    : previous / next channel')
+        mng = plt.get_current_fig_manager()
+        mng.full_screen_toggle()
         plt.show()
         
         
@@ -55,8 +69,15 @@ class RawDataGUI:
                 currStrValues = currLine[:-1].split()
                 for iEntry,key in enumerate(self.channelNames):
                     self.allDataSets[iDataSet][key][iSample] = float(currStrValues[iEntry])
-                    
+
+            while (currLine.find("Sequence: END") == -1):
+                currLine = f.readline()
+                if currLine == "":
+                    break
+                self.allHeaders[iDataSet].append(currLine)                     
+
             iDataSet += 1
+
         f.close()
 
     def keyCallback(self, event):
@@ -78,10 +99,18 @@ class RawDataGUI:
         
     def updateGUI(self):
         self.ax.cla()
+        self.axTxt.cla()
+        self.axTxt.axis("off")
         self.ax.plot(self.allDataSets[self.currentDataset][self.channelNames[self.iChannel]], marker='x')
+        self.ax.set_xlim((0,self.allDataSets[self.currentDataset][self.channelNames[self.iChannel]].shape[0]))
+
+        self.ax.set_title(self.inputFile)
+        self.ax.set_xlabel("Samples")
+        self.ax.set_ylabel(self.channelNames[self.iChannel] )
+        self.axTxt.set_title('Dataset {} / {}'.format(self.currentDataset, self.nDatasets))
+        self.axTxt.text(0, 1, "".join(self.allHeaders[self.currentDataset]), verticalalignment='top')
         self.ax.grid(True)
         self.ax.set_xlim((0,self.allDataSets[self.currentDataset][self.channelNames[self.iChannel]].shape[0]))
-        plt.title('Sequence {} / {}       {}'.format(self.currentDataset, self.nDatasets, self.channelNames[self.iChannel]))
         plt.draw()
 
 
@@ -93,7 +122,8 @@ if len(sys.argv) == 1:
     print('found :{}'.format(fileName[25:]))
 else:
     fileName = sys.argv[1]
-
+    if fileName.find("/") == -1: # no path defined => assume /data/diagnostic_samples/
+        fileName = "/data/diagnostic_samples/" + fileName
 
 # read file and open GUI
 RawDataGUI(fileName)
