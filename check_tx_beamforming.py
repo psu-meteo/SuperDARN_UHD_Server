moved simple (1/nChannels) channel scaling (gain control) from cuda to usrp_server, fixed bug: channels are now removed if controlprogram closes

check_tx_beamforming.py
@@ -118,8 +118,9 @@ antenna_pos_y = np.zeros_like(antenna_pos_x)
 antenna_pos_x -=  np.mean(antenna_pos_x)
 
 if len(usrp_xPos) != nAntennas:
-    print("just {} usrp entries found for {} antennas".format(len(usrp_xPos), nAntennas))
-    raise VauleError("error")
+    raise ValueError("just {} usrp entries found for {} antennas".format(len(usrp_xPos), nAntennas))
+
+
 """
 # plot to check
 plt.figure()

cuda_driver.py
@@ -43,7 +43,7 @@ SIDEB = 1
 RXDIR = 'rx'
 TXDIR = 'tx'
 
-DEBUG = False
+DEBUG = True
 verbose = 1
 C = 3e8
 
@@ -196,6 +196,11 @@ class cuda_generate_pulse_handler(cudamsg_handler):
         # construct baseband tx sample array
         bb_signal = np.complex128(np.zeros((nAntennas, nPulses, len(pulsesamps))))
 
+        self.logger.debug("ChannelScalingFactor is {} for channel {} (tfreq={} MHz). ".format(channel.channelScalingFactor, channel.ctrlprm['channel'],channel.ctrlprm['tfreq']/1000 ))
+
+        if channel.channelScalingFactor == 0:
+            self.logger.warning("ChannelScalingFactor is zero for channel {} (tfreq={} MHz). Channel is muted! ".format(channel.ctrlprm['channel'],channel.ctrlprm['tfreq'] / 1000 ))
+
         for iAntenna in range(nAntennas):
             for iPulse in range(nPulses):
                 # compute pulse compression 
@@ -211,6 +216,9 @@ class cuda_generate_pulse_handler(cudamsg_handler):
                 
                 # apply beamforming
                 psamp *= beamforming_shift[iAntenna]
+
+                # apply gain control
+                psamp *= channel.channelScalingFactor
 
                 # update baseband pulse sample array for antenna
                 bb_signal[iAntenna][iPulse] = psamp
@@ -291,7 +299,7 @@ class cuda_remove_channel_handler(cudamsg_handler):
 
         if channelNumber in self.gpu.channelNumbers:
             chIdx = self.gpu.channelNumbers.index(channelNumber)
-            self.gpu.squences[chIdx] = None
+            self.gpu.sequences[chIdx] = None
             self.gpu.channelNumbers[chIdx] = None
             self.logger.debug("Delete Channel {}  at idx {}. ".format(channelNumber, chIdx))
         else:

drivermsg_library.py
@@ -231,18 +231,23 @@ class cuda_add_channel_command(driver_command):
         for sock in self.clients:
             pickle_send(sock, self.sequence)
 
-# clear all channels from gpu..
+# clear one channels from gpu
+# TODO: just transmit channel number to save time?
 class cuda_remove_channel_command(driver_command):
-    def __init__(self, cudas, swing = 0):
-        driver_command.__init__(self, cudas, CUDA_REMOVE_SEQUENCE)
+    def __init__(self, cudas, sequence = None, swing = 0):
+        driver_command.__init__(self, cudas, CUDA_REMOVE_CHANNEL)
         self.queue(swing, np.uint32, 'swing')
-        pdb.set_trace()
+        self.sequence = sequence 
+       # pdb.set_trace()
 
     def receive(self, sock):
         super().receive(sock)
         self.sequence = pickle_recv(sock)
 
     def transmit(self):
+        if self.sequence == None:
+            print('cannot send undefined sequence to channel')
+            pdb.set_trace()
         super().transmit()
         for sock in self.clients:
             pickle_send(sock, self.sequence)
@@ -338,7 +343,7 @@ class usrp_exit_command(driver_command):
 
 # class with pulse sequence information
 class sequence(object):
-    def __init__(self, npulses, tr_to_pulse_delay, pulse_offsets_vector, pulse_lens, phase_masks, pulse_masks, ctrlprm):
+    def __init__(self, npulses, tr_to_pulse_delay, pulse_offsets_vector, pulse_lens, phase_masks, pulse_masks, channelScalingFactor, ctrlprm):
         self.ctrlprm = ctrlprm
         self.npulses = npulses
         self.pulse_offsets_vector = pulse_offsets_vector
@@ -348,6 +353,7 @@ class sequence(object):
         self.tr_to_pulse_delay = tr_to_pulse_delay
         self.ready = True # TODO: what is ready flag for?
         self.tx_time = self.pulse_lens[0] + 2 * self.tr_to_pulse_delay
+        self.channelScalingFactor = channelScalingFactor # correct frequency dependency and superpositioning of channels
          
         # validate input sequence
         if self.ctrlprm['rfreq'] and self.ctrlprm['rfreq'] != self.ctrlprm['tfreq']: