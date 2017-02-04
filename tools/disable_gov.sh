cb688dd07d3245924c3fdcddcd700e9e7b38b053
radar <radar@kodiak-main.GI.ALASKA.EDU>
2017-02-03 17:00:09 -0900

    removed some unused files, added script to disable frequency scaling on cpu

---
+++ tools/disable_gov.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# script to disable cpu frequency scaling
+for CPUFREQ in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor; do [ -f $CPUFREQ ] || continue; echo -n performance > $CPUFREQ; done
--- usrp_server/run_usrp_server.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#pycallgraph graphviz -- 
-python3 -m cProfile -o cprofile_usrp_server usrp_server.py
