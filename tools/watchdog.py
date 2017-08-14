f0ede6a68c465020782206a4f24d76d072886f8a
mgu <Martin.Guski@gmail.com>
2017-08-13 17:50:06 -0800

    start usrp_driver watchdog part

--- tools/watchdog.py
+++ tools/watchdog.py
@@ -12,8 +12,31 @@ print_status = True
 restart  = False
 
 server_status_file = os.path.join(basePath, 'log', "usrp_server_status.txt")
-check_period = 1 # sec
+check_period = 3 # sec
 file_age_limit = 30 + 10
+
+
+class usrpDriverWatcher():
+   def __init__(self, fileName):
+      self.config = srr.read_config(fileName)
+      usrp_name_list = self.config.sections()
+      self.usrp_list = []
+      for usrpName in usrp_name_list:
+         self.usrp_list.append(usrpClass(self.config[usrpName]))
+
+      
+   
+
+class usrpClass():
+   def __init__(self, configDict):
+      self.host = configDict['usrp_hostname']
+      self.computer = configDict['driver_hostname']
+      self.mainarray = configDict['mainarray']
+      self.side = configDict['side']
+      
+      
+fileName = os.path.join(basePath, 'usrp_config.ini')
+usrp_driver_watcher = usrpDriverWatcher(fileName)
 
 while True:
    os.system('clear')
@@ -29,6 +52,14 @@ while True:
       for line in status:
          print("    " + line[:-1])
       print("\n\n")
+
+
+      print("  usrp_driver:")
+      usrp_processes = srr.get_usrp_driver_processes()
+      for pro in usrp_processes:
+         print("    usrp: {}, antenna: {} PID: {}".format(pro['host'], pro['antenna'], pro['pid']))
+      print("\n\n")
+
 
 #   print("file: {}, now: {}, age: {} s".format(m_time, now, now-m_time))
    if file_age_limit > file_age:
