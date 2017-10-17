dba9ee80c27b0585cae826c8c8fd96311a464eab
radar <radar@kodiak-main.GI.ALASKA.EDU>
2017-10-17 12:45:41 -0800

    forgot header file

--- usrp_driver/tx_worker.h
+++ usrp_driver/tx_worker.h
@@ -1,6 +1,6 @@
 void usrp_tx_worker(
     uhd::tx_streamer::sptr tx_stream,
-    std::vector<std::vector<std::complex<int16_t>>> pulse_samples,
+    std::vector<std::vector<std::complex<int16_t>>> *pulse_samples,
     size_t num_samples_per_pulse,
 //    size_t pulses_per_sequence,
     uhd::time_spec_t burst_start_time,
