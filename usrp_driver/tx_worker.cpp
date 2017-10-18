70e5956bfeba2420645efbc6a9e5ad0a2eca3538
radar <radar@kodiak-main.GI.ALASKA.EDU>
2017-10-17 19:35:43 -0800

    correcting tx_burst_length_samples, rewriting call by ref change

--- usrp_driver/tx_worker.cpp
+++ usrp_driver/tx_worker.cpp
@@ -31,7 +31,7 @@ extern int verbose;
  **********************************************************************/
 void usrp_tx_worker(
     uhd::tx_streamer::sptr tx_stream,
-    std::vector<std::vector<std::complex<int16_t>>> *pulse_samples,
+    std::vector<std::vector<std::complex<int16_t>>> *pulse_samples_pointer,
     size_t padded_num_samples_per_pulse,
 //    size_t pulses_per_sequence,
     uhd::time_spec_t burst_start_time,
@@ -39,6 +39,7 @@ void usrp_tx_worker(
 ){
     // pulse samples are zero padded with 
     // [ spb * 0 ] [ pulse samples ] [ spb * 0 ]
+    std::vector<std::vector<std::complex<int16_t>>> pulse_samples = (*pulse_samples_pointer);
     struct timeval t0, t1;
     gettimeofday(&t0,NULL);
 
@@ -56,31 +57,20 @@ void usrp_tx_worker(
     int32_t samples_per_pulse = padded_num_samples_per_pulse - 2*spb; 
     DEBUG_PRINT("TX_WORKER nSamples_per_pulse=%i + 2*%zu (zero padding)\n", samples_per_pulse, spb);
     int iSide;
-    int nSides = (*pulse_samples).size();
+    int nSides = pulse_samples.size();
     std::vector<std::complex<int16_t>*> buffer(nSides); 
+    DEBUG_PRINT("TX_WORKER nSides=%i\n", nSides);
 
     // assume at least spb length zero padding before first pulse
-    size_t tx_burst_length_samples = pulse_sample_idx_offsets[number_of_pulses-1] + padded_num_samples_per_pulse; 
+    size_t tx_burst_length_samples = pulse_sample_idx_offsets[number_of_pulses-1] + samples_per_pulse -1;
     size_t nacc_samples = 0;
     size_t sample_idx = 0;
     uint32_t pulse_idx = 0;
  
 
     for (iSide =0; iSide<nSides; iSide++) {
-        buffer[iSide] = &((*pulse_samples)[iSide][sample_idx]); 
+        buffer[iSide] = &pulse_samples[iSide][sample_idx]; 
     }
-
-
-    // DEBUG to check timing
-  //  double time_to_start;
-  //  uhd::time_spec_t rx_usrp_pre_stream_time = usrp->get_time_now();
-  //  time_to_start = start_time.get_real_secs() - rx_usrp_pre_stream_time.get_real_secs();
-  //  DEBUG_PRINT("#timing: time left for tx_worker  %f ms", time_to_start*1000);
-
-
-
-
-
     nacc_samples += tx_stream->send(buffer, spb, md, timeout);
 
     md.start_of_burst = false;
@@ -97,13 +87,19 @@ void usrp_tx_worker(
         
         // if the transmit pulse will arrive within the current sample packet, calculate correct sample index into sample vector
         if(nsamples_to_send >= samples_to_pulse) {
+//            DEBUG_PRINT("pulse_idx=%i, nacc_samples=%i, nsamples_to_send=%d, samples_to_pulse=%d \n", pulse_idx, nacc_samples, nsamples_to_send, samples_to_pulse);
             if(samples_to_pulse * -1 < samples_per_pulse) {
                 sample_idx = spb - samples_to_pulse + (pulse_idx) * padded_num_samples_per_pulse;
             } else {
                 // if we've passed the tail of the pulse, then restart and look for the next one..
                 // DEBUG_PRINT("pulse idx: %d complete\n", pulse_idx);
-                pulse_idx++;
-                continue;
+         //       if (number_of_pulses -1 > pulse_idx){
+                    pulse_idx++;
+                    continue;
+         //       } else {
+         //         sample_idx = 0;
+
+         //       }
             }
         }
 
@@ -114,7 +110,7 @@ void usrp_tx_worker(
 
         //DEBUG_PRINT("sending buffer with sample_idx: %d\n", sample_idx);
         for (iSide =0; iSide<nSides; iSide++) {
-            buffer[iSide] = &((*pulse_samples)[iSide][sample_idx]); 
+            buffer[iSide] = &pulse_samples[iSide][sample_idx]; 
         }
 
 
@@ -127,9 +123,10 @@ void usrp_tx_worker(
     //    if(DEBUG && sample_idx) std::cout << boost::format(" Sent packet:  idx: %i") % sample_idx << std::endl;
         nacc_samples += ntx_samples;
     }
+    DEBUG_PRINT("TX_WORKER tx_burst_length_samples=%i\n", tx_burst_length_samples );
 
     md.end_of_burst = true;
-    tx_stream->send(buffer, 0, md, timeout);
+    tx_stream->send(&pulse_samples[0], 0, md, timeout);
 
     DEBUG_PRINT("Waiting for async burst ACK... ");
     uhd::async_metadata_t async_md;
