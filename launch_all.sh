0d7a5f4a1d625937019242bdbe0527cb1db7f0cd
radar <radar@kodiak-main.GI.ALASKA.EDU>
2017-10-19 11:25:11 -0800

    start and stop scripts for all processes

--- launch_all.sh
+++ launch_all.sh
@@ -13,8 +13,8 @@ fitacfwrite -r kod.c -lp 43103 -ep 43000 &
 rtserver -rp 41104 -ep 41000 -tp 1401 & # ch 4
 rtserver -rp 43104 -ep 43000 -tp 1402 & # ch 3
 
-# ssh radar@kodiak-aux 'python3 /home/radar/SuperDARN_UHD_Server/tools/srr_watchdog.py &' &
-./home/radar/repos/usrp/tools/srr_watchdog.py server &
+ssh radar@kodiak-aux 'python3 /home/radar/SuperDARN_UHD_Server/tools/srr_watchdog.py &' &
+python3 /home/radar/repos/usrp/tools/srr_watchdog.py server &
 
 sleep 15
 schedule -l /data/ros/scdlog -f /data/ros/scd/kod.d.scd & #TODO add channel c
