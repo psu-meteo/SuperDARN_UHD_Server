f168b6296aa13f556deff6dd6b4e7545389e5d8b
radar <radar@kodiak-main.GI.ALASKA.EDU>
2017-10-19 13:16:36 -0800

    don't add result dict if not triggered

--- usrp_server/usrp_server.py
+++ usrp_server/usrp_server.py
@@ -1339,32 +1339,33 @@ class RadarHardwareManager:
               cmd.client_return()
               self.logger.debug("end USRP_GET_TIME")
 
-           nSamples_rx_requested_of_last_trigger = channel.nrf_rx_samples_per_integration_period
+              nSamples_rx_requested_of_last_trigger = channel.nrf_rx_samples_per_integration_period
 
-           # calculate sequence times for control program
-           sequence_start_time_secs  = np.zeros(self.nSequences_per_period, dtype=np.uint64)
-           sequence_start_time_usecs = np.zeros(self.nSequences_per_period, dtype=np.uint32)
-           for iSequence in range(self.nSequences_per_period):
-               pulse_start_time = usrp_integration_period_start_clock_time + iSequence * self.nsamples_per_sequence / self.usrp_rf_rx_rate
-               sequence_start_time_secs[iSequence]  = int(pulse_start_time) 
-               sequence_start_time_usecs[iSequence] = int(( pulse_start_time - int(pulse_start_time) ) *1e6)
+              # calculate sequence times for control program
+              n_sequence_times = max(self.nSequences_per_period,1) # at least one time to transmit to control program
+              sequence_start_time_secs  = np.zeros(n_sequence_times, dtype=np.uint64)
+              sequence_start_time_usecs = np.zeros(n_sequence_times, dtype=np.uint32)
+              for iSequence in range(n_sequence_times):
+                  pulse_start_time = usrp_integration_period_start_clock_time + iSequence * self.nsamples_per_sequence / self.usrp_rf_rx_rate
+                  sequence_start_time_secs[iSequence]  = int(pulse_start_time) 
+                  sequence_start_time_usecs[iSequence] = int(( pulse_start_time - int(pulse_start_time) ) *1e6)
 
-           # save data for returning results to control program
-           resultDict = {}
-           resultDict['sequence_start_time_secs']      = sequence_start_time_secs
-           resultDict['sequence_start_time_usecs']     = sequence_start_time_usecs
-           resultDict['number_of_samples']             = self.commonChannelParameter['number_of_samples']
-           resultDict['nSequences_per_period']         = self.nSequences_per_period       
-           resultDict['pulse_sequence_offsets_vector'] = self.commonChannelParameter['pulse_sequence_offsets_vector'] 
-           resultDict['npulses_per_sequence']          = self.commonChannelParameter['npulses_per_sequence']
-           resultDict['results_are_valid']             = True
-           for channel in self.channels: 
-              if (channel is not None) and (not channel.scanManager.isLastPeriod): 
-                  resultDict['nbb_rx_samples_per_sequence'] = channel.nbb_rx_samples_per_sequence
-                  resultDict['pulse_lens']                  = channel.pulse_lens    
-                  channel.resultDict_list.insert(0,resultDict.copy())
+              # save data for returning results to control program
+              resultDict = {}
+              resultDict['sequence_start_time_secs']      = sequence_start_time_secs
+              resultDict['sequence_start_time_usecs']     = sequence_start_time_usecs
+              resultDict['number_of_samples']             = self.commonChannelParameter['number_of_samples']
+              resultDict['nSequences_per_period']         = self.nSequences_per_period       
+              resultDict['pulse_sequence_offsets_vector'] = self.commonChannelParameter['pulse_sequence_offsets_vector'] 
+              resultDict['npulses_per_sequence']          = self.commonChannelParameter['npulses_per_sequence']
+              resultDict['results_are_valid']             = True
+              for channel in self.channels: 
+                 if (channel is not None) and (not channel.scanManager.isLastPeriod): 
+                     resultDict['nbb_rx_samples_per_sequence'] = channel.nbb_rx_samples_per_sequence
+                     resultDict['pulse_lens']                  = channel.pulse_lens    
+                     channel.resultDict_list.insert(0,resultDict.copy())
 
-           if trigger_next_period: 
+         #  if trigger_next_period: 
               # broadcast the start of the next integration period to all usrp
               self.logger.debug('start USRP_TRIGGER')
               trigger_time = usrp_time + INTEGRATION_PERIOD_SYNC_TIME 
@@ -1714,7 +1715,7 @@ class RadarHardwareManager:
             bb_samplingRate = RHM.commonChannelParameter['baseband_samplerate'] 
             offset = int(np.round(900e-6*bb_samplingRate))
             channel = RHM.channels[0]
-            pulse_offsets = RHM.all_possible_integration_period_pulse_sample_offsets[:channel.resultDict_list[-1]['npulses_per_sequence'] *  channel.resultDict_list[-1]['nSequences_per_period']]
+            pulse_offsets = RHM.all_possible_integration_period_pulse_sample_offsets
             pulse_offsets = np.array(np.round(pulse_offsets/RHM.usrp_rf_rx_rate*bb_samplingRate), dtype=np.int)
             pulse_offsets -= pulse_offsets[0]
             rx_idx = []
@@ -1722,6 +1723,7 @@ class RadarHardwareManager:
                 rx_idx += range(pulse_offsets[iPulse]+offset, pulse_offsets[iPulse+1]-offset)
             rx_idx += range(pulse_offsets[iPulse+1]+offset, nSamples)
             rx_idx = np.array(rx_idx, dtype=np.int)
+            rx_idx = rx_idx[rx_idx<nSamples-1]
             if debugPlot:
                 import matplotlib.pyplot as plt
 
