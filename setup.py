97a18a524b9645183d5362bafb78a97a743611ba
bgklug <bgklug@alaska.edu>
2019-04-15 14:13:50 -0800

    Fix matplotlib name

--- setup.py
+++ setup.py
@@ -6,9 +6,12 @@ setup(
     name="SuperDARN_UHD_Server",
     version="1.0",
     python_requires=">=3.3.0",
+    url="https://github.com/UAF-SuperDARN-OPS/SuperDARN_UHD_Server",
+    maintainer="UAF SuperDARN",
+    maintainer_email="bgklug@alaska.edu",
     packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
-    install_requirements=[
-        'pycuda', 'numpy', 'scipy', 'numexpr', 'posix-ipc', 'termcolor', 'matplotllib',
+    install_requires=[
+        'pycuda', 'numpy', 'scipy', 'numexpr', 'posix-ipc', 'termcolor', 'matplotlib',
         'pytest'
     ]
 )
