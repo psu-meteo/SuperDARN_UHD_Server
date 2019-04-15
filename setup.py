2b2850fb91fb56ea8e0bab8058d283d6ea7237fe
bgklug <bgklug@alaska.edu>
2019-04-15 11:32:50 -0800

    Add installation package requirements

--- setup.py
+++ setup.py
@@ -5,5 +5,10 @@ from setuptools import setup, find_packages
 setup(
     name="SuperDARN_UHD_Server",
     version="1.0",
-    packages=find_packages()
+    python_requires=">=3.3.0",
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+    install_requirements=[
+        'pycuda', 'numpy', 'scipy', 'numexpr', 'posix-ipc', 'termcolor', 'matplotllib',
+        'pytest'
+    ]
 )
